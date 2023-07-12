4320d770b1e071bae01299b24a4680de13e6efb8
matkofbass <xdapwou6qkyil45pkqlmbcu4omy4cmtxab27nfdky6e2tsw5phea>
2023-07-12 13:43:56 +0000

    upload update

--- middleware/fileMiddleware.js
+++ middleware/fileMiddleware.js
@@ -1,9 +1,12 @@
 const multer = require('multer');
 
 const storage = multer.diskStorage({
+
+  /*
     destination: function (req, file, cb) {
       cb(null, 'uploads/'); // Specify the destination folder for uploaded files
     },
+    */
     filename: function (req, file, cb) {
       cb(null, file.originalname); // Use the original file name for storage
     }
